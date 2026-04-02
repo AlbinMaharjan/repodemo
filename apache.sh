6f85bf4e6a9a0df2f5a6bd1844aec079d502cc1c
Albin Maharjan <albinmaharjan20@gmail.com>
2026-04-02 10:56:54 +0000

    initial conflit resolving

--- apache.sh
+++ apache.sh
@@ -1,11 +1,17 @@
+<<<<<<< HEAD
 !bin/bash/
 
+=======
+#!bin/bash/
+<<<<<<< HEAD
+>>>>>>> feature2
 echo "installing apache 2"
 sudo apt update 
 sudo apt install apache2
 
 
 
+<<<<<<< HEAD
 
 #print hello world
 echo "hello world"
@@ -13,3 +19,10 @@ echo "hello world"
 echo "installing apache 2""
 
 
+=======
+#print hello world
+echo "hello world"
+=======
+echo "installing apache 2""
+>>>>>>> 0d35055... changed quotation
+>>>>>>> feature2
